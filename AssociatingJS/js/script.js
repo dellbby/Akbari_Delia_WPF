886cc159640646daadcf8ef75c7490150c349680
dellbby <dellbby4@yahoo.com>
2014-05-14 22:56:50 -0700

    Video: Values and Variables
    Notes and examples from videos in script.js

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,1 +1,17 @@
+//JavaScript
+
+var age; //declaration
+age =22; //definition
+
+var yearOfBirth = 1991; //declaring and defining on the same line
+
+var kermit = "my friend"; 
+var phrase = "I don't know";
+
+var firstName = "Kermit";
 alert("Hello World! This is in the javascript file");
+
+//Arrays
+
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"];
+console.log(muppetNames[0]);
