4c130d40d5f0a8c64f7e3ddcb993e79b9ae76260
dellbby <dellbby4@yahoo.com>
2014-05-15 00:06:16 -0700

    Expressions Worksheet - update 1

---
+++ Expressions_worksheet/jsscript/script.js
@@ -0,0 +1,29 @@
+//JavaScript
+// Delia Akbari - May 13, 2014 - Expressions Worksheet
+
+//Examples:
+//Calculating the area of a rectangle given the width and height
+var width = 5;
+var height =65;
+//width x height / 2 = area
+var area = width * height/2;
+//print out the output area
+console.log(area); 
+
+
+
+//Problem: Dog Years
+//Dogs age 7 times fast than humans, so a dog that is 1 year old in human years is 7 years old in dog years
+//calculate how old sparky is in dog years based on his age
+//Sparky's age is 2
+var age;
+age = 2;
+var age = age * 7;
+console.log(age);
+
+
+//Slice of Pie Part 1
+//number of slices per pizza - 10
+//number of people at the party - 50
+//number of pizzas ordered - 5
+var 
