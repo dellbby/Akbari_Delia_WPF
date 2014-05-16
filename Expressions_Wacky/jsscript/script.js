ddb790eccafe9776395dc18291cdad6fea39cf53
dellbby <dellbby4@yahoo.com>
2014-05-15 20:09:01 -0700

    Changed wording

--- Expressions_Wacky/jsscript/script.js
+++ Expressions_Wacky/jsscript/script.js
@@ -3,11 +3,11 @@
 
 //Story about me with Strings
 
-var firstName = "Delia"; //firstname 
-var lastName = "Akbari"; //lastname
-var dog = "Booboo"; //my dogs name
-var food = "Kabob"; // my favorite food
-var sport = "Basketball"; // my favorite sport
+var firstName = "Pepper"; //firstname 
+var lastName = "Pinapple"; //lastname
+var dog = "Walter"; //my dogs name
+var food = "Funnel Cakes from Six Flags"; // my favorite food
+var sport = "Beer Pong"; // my favorite sport
 
-console.log( " Hello! My name is "+firstName+ " and my last name is "  +lastName+ ". I have a dog, her name is " +dog+ ". My favorite type of food is " +food+ ". I do not play sports, but if I did, I would play "+sport+ " because many people think I have the perfect height for it.")
+console.log( " Hello! My name is "+firstName+ " and my last name is "  +lastName+ ". I have a dog, her name is " +dog+ " (yes, I did give my dog a boy's name). My favorite type of food is " +food+ ". I do not play sports, but if I did, I would play "+sport+ " because I have perfec aim.")
 
