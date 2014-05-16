bf5cbc11d71f065851a8ede1888f7e7db7029019
dellbby <dellbby4@yahoo.com>
2014-05-15 19:27:27 -0700

    Adding 3 Factors and givens to script for industry

--- Expressions_Industry/jsscript/script.js
+++ Expressions_Industry/jsscript/script.js
@@ -1,13 +1,14 @@
 //JavaScript
 // Delia Akbari - May 14, 2014 - Expressions industry
 
-//Calculating How much I make in a week.
+//Calculating How much I make in a week and in a month
 var perhour;
-perhour = 12;
-//i make 12 per hour
+perhour = 12;  //i make 12 per hour
 var hours;
-hours = 40;
-var total = perhour* hours;
+hours = 40; // I work 40 hours per week
+var total = perhour* hours;  // this code explains that my hours multiplied by the amount that I made equals how much I make per week.
+var monthly = total * 4; 
 
 
-console.log("I make a total of $" +total+" per week.");
+
+console.log("I work a total of " +hours+ " hours per week. I make $"+perhour+ " per hour and make a total of $" +total+" per week. I also make $" +monthly+ " per month.");
