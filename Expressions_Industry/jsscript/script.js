5d459819472313f7deb9a7c2bb5e57027abec7ee
dellbby <dellbby4@yahoo.com>
2014-05-15 19:28:36 -0700

    Added comments to industry expression script

--- Expressions_Industry/jsscript/script.js
+++ Expressions_Industry/jsscript/script.js
@@ -7,8 +7,5 @@ perhour = 12;  //i make 12 per hour
 var hours;
 hours = 40; // I work 40 hours per week
 var total = perhour* hours;  // this code explains that my hours multiplied by the amount that I made equals how much I make per week.
-var monthly = total * 4; 
-
-
-
+var monthly = total * 4;  // this code explains that the total amount I make per month is the total I make per week multiplied by 4. 
 console.log("I work a total of " +hours+ " hours per week. I make $"+perhour+ " per hour and make a total of $" +total+" per week. I also make $" +monthly+ " per month.");
