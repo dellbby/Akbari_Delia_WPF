0cb81abfdb48e9c45191508b479f113c21771e1e
dellbby <dellbby4@yahoo.com>
2014-05-15 20:02:30 -0700

    string - 2

--- Expressions_Wacky/jsscript/script.js
+++ Expressions_Wacky/jsscript/script.js
@@ -9,5 +9,5 @@ var dog = "Booboo"; //my dogs name
 var food = "Kabob"; // my favorite food
 var sport = "Basketball"; // my favorite sport
 
-console.log( " Hello! My name is "+firstName+ " and my last name is "  +lastName+ ". I have a dog, her name is " +dog+ ". My favorite type of food is " +food+ ". I do not play sports, but if I did, I would play "+sport+ " because I am tall.")
+console.log( " Hello! My name is "+firstName+ " and my last name is "  +lastName+ ". I have a dog, her name is " +dog+ ". My favorite type of food is " +food+ ". I do not play sports, but if I did, I would play "+sport+ " because many people think I have the perfect height for it.")
 
