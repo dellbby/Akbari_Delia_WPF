308adc2e91af424a4102345e58e38c9f02a032ec
dellbby <dellbby4@yahoo.com>
2014-05-15 20:10:13 -0700

    Wacky - update

--- Expressions_Wacky/jsscript/script.js
+++ Expressions_Wacky/jsscript/script.js
@@ -9,5 +9,5 @@ var dog = "Walter"; //my dogs name
 var food = "Funnel Cakes from Six Flags"; // my favorite food
 var sport = "Beer Pong"; // my favorite sport
 
-console.log( " Hello! My name is "+firstName+ " and my last name is "  +lastName+ ". I have a dog, her name is " +dog+ " (yes, I did give my dog a boy's name). My favorite type of food is " +food+ ". I do not play sports, but if I did, I would play "+sport+ " because I have perfec aim.")
+console.log( " Hello! My name is "+firstName+ " and my last name is "  +lastName+ ". I have a dog, her name is " +dog+ " (yes, I did give my dog a boy's name). My favorite type of food is " +food+ ". I do not play sports, but if I did, I would play "+sport+ " because I have perfec aim. P.S: The GIF on my index page is really not helping me concentrate ^__^." )
 
