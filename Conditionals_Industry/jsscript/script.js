a3c487ccc2737f52281b303d64d4a2fc42b8094b
dellbby <dellbby4@yahoo.com>
2014-05-21 23:15:55 -0700

    Industry code changes

--- Conditionals_Industry/jsscript/script.js
+++ Conditionals_Industry/jsscript/script.js
@@ -1,3 +1,16 @@
 //JavaScript
 // Delia Akbari - May 21, 2014 - Conditional Industry
+//If we have enough money saved, me and my co-worker can fly to Paris for a conference.
+
+var ticketPriceForTwo = 400; 
+var poccketmoney = 250;
+var coworkerPocketMoney = 145;
+
+if(ticketPriceForTwo < coworkerPocketMoney && coworkerPocketMoney > 400) {
+	console.log("We can buy the tickets!")
+}else{
+		console.log("No conference in Paris, sorry.");
+}
+
+//
 
