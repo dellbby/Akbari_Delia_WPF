a0bb24be6ed6b87d261d4926273ba78b819194fd
dellbby <dellbby4@yahoo.com>
2014-05-21 23:05:37 -0700

    Conditional - industry

--- Conditionals_Industry/jsscript/script.js
+++ Conditionals_Industry/jsscript/script.js
@@ -1,11 +1,3 @@
 //JavaScript
-// Delia Akbari - May 14, 2014 - Expressions industry
+// Delia Akbari - May 21, 2014 - Conditional Industry
 
-//Calculating How much I make in a week and in a month
-var perhour;
-perhour = 12;  //i make 12 per hour
-var hours;
-hours = 40; // I work 40 hours per week
-var total = perhour* hours;  // this code explains that my hours multiplied by the amount that I made equals how much I make per week.
-var monthly = total * 4;  // this code explains that the total amount I make per month is the total I make per week multiplied by 4. 
-console.log("I work a total of " +hours+ " hours per week. I make $"+perhour+ " per hour and make a total of $" +total+" per week. I also make $" +monthly+ " per month.");
