b2e2d1c0ab5694ff96bae4e0614a9c1fb65842ce
dellbby <dellbby4@yahoo.com>
2014-05-21 23:18:21 -0700

    pocket money

--- Conditionals_Industry/jsscript/script.js
+++ Conditionals_Industry/jsscript/script.js
@@ -3,7 +3,7 @@
 //If we have enough money saved, me and my co-worker can fly to Paris for a conference.
 
 var ticketPriceForTwo = 400;  //Price for two tickets
-var poccketmoney = 250;
+var poccketmoney = 250; //Amount of Money I have in my wallet
 var coworkerPocketMoney = 145;
 
 if(ticketPriceForTwo < coworkerPocketMoney && coworkerPocketMoney > 400) {
