b318cfdca44979de68d369dceeb40bbfeeaf73e9
dellbby <dellbby4@yahoo.com>
2014-05-21 23:18:35 -0700

    coworker money

--- Conditionals_Industry/jsscript/script.js
+++ Conditionals_Industry/jsscript/script.js
@@ -4,7 +4,7 @@
 
 var ticketPriceForTwo = 400;  //Price for two tickets
 var poccketmoney = 250; //Amount of Money I have in my wallet
-var coworkerPocketMoney = 145;
+var coworkerPocketMoney = 145; //Amount of money my coworker has
 
 if(ticketPriceForTwo < coworkerPocketMoney && coworkerPocketMoney > 400) {
 	console.log("We can buy the tickets! Pack your bags!")
