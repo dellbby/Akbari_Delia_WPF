30b0f1d19beef0f5f9d224b7ce7d0a5a94cc05a3
dellbby <dellbby4@yahoo.com>
2014-05-21 22:34:00 -0700

    Script - PErsonal

---
+++ Conditionals_Personal/jsscript/script.js
@@ -0,0 +1,8 @@
+//JavaScript
+// Delia Akbari - May 21, 2014 - Conditional Personal
+//Am I going to do my photoshoot on Saturday if the weather is nice?
+
+var photoshootWeather = 73;
+//if the weather is 73 or higher, I will do the photoshoot 
+
+(photoshootWeather) ? console.log ("You can have the photoshoot!") : console.log("You can't have the photoshoot :( );
