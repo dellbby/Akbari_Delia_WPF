c1c27300dcf14933f727d1b207c06e47193d139d
dellbby <dellbby4@yahoo.com>
2014-05-21 23:20:06 -0700

    notes

--- Conditionals_Industry/jsscript/script.js
+++ Conditionals_Industry/jsscript/script.js
@@ -6,7 +6,7 @@ var ticketPriceForTwo = 400;  //Price for two tickets
 var poccketmoney = 250; //Amount of Money I have in my wallet
 var coworkerPocketMoney = 145; //Amount of money my coworker has
 
-if(ticketPriceForTwo < coworkerPocketMoney && coworkerPocketMoney > 400) {
+if(ticketPriceForTwo < coworkerPocketMoney && coworkerPocketMoney > 400) { //Used Relational Operator &&
 	console.log("We can buy the tickets! Pack your bags!") // if total is 400 or more, you can go
 }else{
 		console.log("No conference in Paris, sorry."); //If total is less than 400, you can't go
