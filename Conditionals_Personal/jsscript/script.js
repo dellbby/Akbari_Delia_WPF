fa0182af9ae45fb121458e1b3a25ae467bffabae
dellbby <dellbby4@yahoo.com>
2014-05-21 22:53:48 -0700

    changed idea for personal code

--- Conditionals_Personal/jsscript/script.js
+++ Conditionals_Personal/jsscript/script.js
@@ -2,7 +2,15 @@
 // Delia Akbari - May 21, 2014 - Conditional Personal
 //Am I going to do my photoshoot on Saturday if the weather is nice?
 
-var photoshootWeather = 73; //photoshoot weather has to be 73 degrees or higher
-var anotherday = Sunday;  //if the weather is not hot enough, we will move the shoot to sunday
+//var photoshootWeather = 73; //photoshoot weather has to be 73 degrees or higher
+//var anotherday = sunday;  //if the weather is not hot enough, we will move the shoot to sunday
 
-(photoshootWeather < 72) ? console.log ("You can have the photoshoot on Saturday because the weather is " +photoshootWeather+ " degrees!") : console.log("You can't have the photoshoot because it's going to be less than " +photoshootWeather+ " degrees, you can have it on " +anotherday+ "  ");
+//(photoshootWeather < 72) ? console.log ("You can have the photoshoot on Saturday because the weather is " +photoshootWeather+ " degrees!") : console.log("You can't have the photoshoot because it's going to be less than " +photoshootWeather+ " degrees, you can have it on " +anotherday+ "  ");
+
+//If I weight less than 140, I can wear the blue dress, If I weight less than 130, I can wear the pink dress.
+var weight = 135; //current weight
+var dresss;
+
+dress = ( weight < 140) ? "Blue Dress" : "Pink Dress";
+console.log("You can wear the " +dress+ "!.");
+
