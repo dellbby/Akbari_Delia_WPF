0f7387283db03535c469343e1002e3cc282c4eb5
dellbby <dellbby4@yahoo.com>
2014-05-21 22:45:57 -0700

    change code

--- Conditionals_Personal/jsscript/script.js
+++ Conditionals_Personal/jsscript/script.js
@@ -2,7 +2,7 @@
 // Delia Akbari - May 21, 2014 - Conditional Personal
 //Am I going to do my photoshoot on Saturday if the weather is nice?
 
-var photoshootWeather = 73;
-//if the weather is 73 or higher, I will do the photoshoot 
+var photoshootWeather = 73; //photoshoot weather has to be 73 degrees or higher
+var anotherday = Sunday;  //if the weather is not hot enough, we will move the shoot to sunday
 
-(photoshootWeather > 74) ? console.log ("You can have the photoshoot!") : console.log("You can't have the photoshoot :(" );
+(photoshootWeather < 72) ? console.log ("You can have the photoshoot on Saturday because the weather is " +photoshootWeather+ " degrees!") : console.log("You can't have the photoshoot because it's going to be less than " +photoshootWeather+ " degrees, you can have it on " +anotherday+ "  ");
