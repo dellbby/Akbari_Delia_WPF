acbf7b0f710aed0d5125c7a55787e9e6c0256a49
dellbby <dellbby4@yahoo.com>
2014-05-22 19:30:34 -0700

    Conditionals - Worksheet

---
+++ Conditionals_worksheet/jsscript/script.js
@@ -0,0 +1,14 @@
+//JavaScript
+// Delia Akbari - May 21, 2014 - Conditionals - Wacky
+//What to do on a cruise ship, depending on your age!
+var kidAge = 10;
+var teenAge = 21;
+
+//if the person is old enough, they can go to the club, if not, they can go watch the Children's Show
+
+if(kidAge < teenAge) {
+	console.log ("You can go to the club!");
+}else{
+	//if you arent old enough, you go to the Children's show
+	console.log ("You can go to the Children's Show!");
+}
