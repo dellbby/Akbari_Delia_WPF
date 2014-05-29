43ac1d6ffcd442a0d6db66acedb21bff1c1f28d6
dellbby <dellbby4@yahoo.com>
2014-05-28 20:04:29 -0700

    Parameters and argument notes

--- functionnotes.js
+++ functionnotes.js
@@ -1,5 +1,7 @@
 // Functions - Basic Structure
 
+var width = 5;
+
 function outptMsg() {
 	console.log("Hello World");
 	
@@ -10,5 +12,20 @@ function calcArea(){
 	var area = width * height;
 	console.log(area);
 }
-
+//invoking function 
 calcArea();
+
+
+//Parameters & Argument Notes
+
+funcName (argument1, argument2);
+function funcName (parameter1, parameter2) {
+	//code the function runs 
+}
+
+
+calcArea(30, 20);
+function calcArea(w, h) { //w = 30, h = 20
+	var area = w*h;
+	console.log(area);
+}
