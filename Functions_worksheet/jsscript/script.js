2d6ae04029ffc0d3480616337912c3baa9e7d79c
dellbby <dellbby4@yahoo.com>
2014-05-28 20:51:15 -0700

    setting up example

--- Functions_worksheet/jsscript/script.js
+++ Functions_worksheet/jsscript/script.js
@@ -1,3 +1,13 @@
 //JavaScript
 // Delia Akbari - May 28, 2014 - Functions Worksheet
 
+//Calculate the Area of a Rectangle
+var width = 5;
+var height = 6;
+var area = calculateArea(width, height);
+console.log("It takes "++ " bee stings to kill ____ animal.");
+
+function calculateArea(w, h )
+{
+     return w *h;
+}
