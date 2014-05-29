438b22922f1aa87e70fecef86dea83b4bce6c0b9
dellbby <dellbby4@yahoo.com>
2014-05-28 21:14:53 -0700

    notes

--- Functions_worksheet/jsscript/script.js
+++ Functions_worksheet/jsscript/script.js
@@ -6,10 +6,10 @@ var victimsWeight = 5; //Weight of animal
 var numberOfStings = 8.666666667; //number of stings per pound
 var stings = victimsWeight * numberOfStings; //Weight times Stings
 
-
+//code function runs
 function calculateStings(w, s )
 {
-     return w * s;
+     return w * s; //storage container and passed in
 }
 
 console.log ("It takes " +stings+ " bee stings to kill a bird."); //print out
