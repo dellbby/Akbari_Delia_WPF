dbc0bfb4ce903f38cce496434dcf07348449a008
dellbby <dellbby4@yahoo.com>
2014-05-28 20:59:34 -0700

    stings

--- Functions_worksheet/jsscript/script.js
+++ Functions_worksheet/jsscript/script.js
@@ -4,11 +4,12 @@
 //Calculate the Area of a Rectangle
 var victimsWeight = 5; //Weight of animal
 var numberOfStings = 8.666666667; //number of stings per pound
-var area = calculateStings(victimsWeight, numberOfStings);
+var stings = victimsWeight * numberOfStings;
 
-console.log("It takes " +return+ " bee stings to kill a bird.");
 
 function calculateStings(w, s )
 {
      return w * s;
 }
+
+console.log ("It takes " +stings+ " bee stings to kill a bird.");;
