6e86de0ced6cb5556c47b8ba9f2f12fe61b213f5
dellbby <dellbby4@yahoo.com>
2014-05-29 16:09:41 -0700

    changed

--- Functions_Industry/jsscript/script.js
+++ Functions_Industry/jsscript/script.js
@@ -3,8 +3,12 @@
 
 //If you work overtime, you get +2 each hour.
 
-var hoursworked = 40;
-var overtime = 2;
-
-if(hoursworked > 40) {
-	//if hours worked is more than 41, then you get $2.00 extra for every hour you work.
+if() {
+	
+console.log("");	
+	
+}else{
+	
+	console.log("");
+	
+}
