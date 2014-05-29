4f1dde97bf6e973ff7a1711a36e7425891597e07
dellbby <dellbby4@yahoo.com>
2014-05-28 23:09:26 -0700

    preparing code

--- Functions_Industry/jsscript/script.js
+++ Functions_Industry/jsscript/script.js
@@ -1,15 +1,33 @@
 //JavaScript
-// Delia Akbari - May 28, 2014 - Functions Worksheet
+// Delia Akbari - May 28, 2014 - Functions Industry
+//Anonymous function
+//Work requires me to drive to Irwindale from Glendale once a week. The drive is 25 miles, I get .35 cents for every mile. How much do I get every month?
 
-//Calculate the Area of a Rectangle
-var victimsWeight = 5; //Weight of animal
-var numberOfStings = 8.666666667; //number of stings per pound
-var stings = victimsWeight * numberOfStings; //Weight times Stings
+var timesPerWeek = prompt("How many times do you drive outside of your work per week?");
+var miles = prompt("How many miles is the drive?");
+var moneyBack = prompt("How much money do you get back per mile driven?");
 
-//code function runs
-function calculateStings(w, s )
-{
-     return w * s; //storage container and passed in
+var totalPerWeek = timesPerWeek * miles * moneyBack;
+
+console.log ("You would get $" +totalPerWeek+ " back every month");
+
+
+
+
+var userInput = prompt("Enter your year of birth");
+console.log (userInput);
+
+////////////////////////////////////////////////////
+var functionName = function(){ //code to run
 }
+functionName();
 
-console.log ("It takes " +stings+ " bee stings to kill a bird."); //print out
+var calcArea = function(timesPerWeek, miles ) {
+	//code function runs
+	var area = timesPerWeek * miles * .35;
+	return area;
+}
+var a = calcArea(4, 25); //invoking happens after defineing function
+console.log ("I work");
+
+
