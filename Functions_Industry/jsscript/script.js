03b301c906ca126fd747d10afa3ae2c0cd775a1a
dellbby <dellbby4@yahoo.com>
2014-05-30 22:14:37 -0700

    code changes

--- Functions_Industry/jsscript/script.js
+++ Functions_Industry/jsscript/script.js
@@ -1,20 +1,13 @@
- //////////
-	 //JavaScript
+//JavaScript
 // Delia Akbari - May 28, 2014 - Functions Industry
 
-// Create a function that gives us a random number
-
-
-//Prompt the user for a minimum value 
-
-//Create a variable to hold min
-var hours = prompt("Please type in how many hours you want to work");
+//Create a variable to hold hours want to work
+var hours = prompt("Please type in how many hours you want to work"); //hours want to work
 
 //double check that min was recorded
 console.log(hours);
 
 //validate that the user typed in a number and didnt leave it blank
-//isNAN () - true if it is a text string, false if it is a number
 //if text string is a number OR equals to nothing
 while(isNaN(hours) || hours === "") {
 	
